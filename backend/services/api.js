24def1081e1379fb7631660412832ad94ce06bef
Janardhanareddy143 <157009810+Janardhanareddy143@users.noreply.github.com>
2025-03-17 10:23:59 +0530

    fix back

--- backend/services/api.js
+++ backend/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://54.123.45.67/api"; // Change this if using a different server
+const API_URL = "http://localhost:5000/api"; // Change this if using a different server
 
 export const createUser = async (userData) => {
   return axios.post(`${API_URL}/users`, userData);
